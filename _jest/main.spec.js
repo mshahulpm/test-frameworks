5468d4160721f1c67b472bb2ed4f1fab48797208
mshahulpm <mshahulpm@gmail.com>
2022-05-11 19:51:03 +0530

    jest testing framework

---
+++ _jest/main.spec.js
@@ -0,0 +1,45 @@
+const { sum } = require('./main');
+
+test('adds 1 + 2 equals to 3', () => {
+    expect(sum(1, 2)).toBe(3);
+})
+
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+})
+
+
+// array 
+const shoppingList = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'milk',
+];
+
+test('the shopping list has milk on it', () => {
+    expect(shoppingList).toContain('milk');
+    expect(new Set(shoppingList)).toContain('milk');
+});
+
+
+// exception 
+
+function compileAndroidCode() {
+    throw new Error('you are using the wrong JDK');
+}
+
+test('compiling android goes as expected', () => {
+    expect(() => compileAndroidCode()).toThrow();
+    expect(() => compileAndroidCode()).toThrow(Error);
+
+    // You can also use the exact error message or a regexp
+    expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
+    expect(() => compileAndroidCode()).toThrow(/JDK/);
+});
